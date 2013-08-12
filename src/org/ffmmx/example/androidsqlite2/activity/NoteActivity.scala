eb2129ec0babfdfb58f206dfd892a5321f46614c
firefoxmmx2 <firefoxmmx2>
2013-08-13 00:42:03 +0800

    添加scala版本的配置文件和类文件

---
+++ src/org/ffmmx/example/androidsqlite2/activity/NoteActivity.scala
@@ -0,0 +1,16 @@
+package org.ffmmx.example.androidsqlite2.activity
+
+import android.app.Activity
+import android.os.Bundle
+import org.ffmmx.example.androidsqlite2.R
+
+/**
+ * 
+ * scala版本的NoteActivity
+ */
+class NoteActivity extends Activity {
+	override def onCreate(savedInstanceState:Bundle):Unit= {
+	  super.onCreate(savedInstanceState)
+	  setContentView(R.id.)
+	}
+}
