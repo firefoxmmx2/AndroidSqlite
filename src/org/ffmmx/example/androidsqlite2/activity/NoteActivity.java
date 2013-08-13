37275bdba826f6cbc36c44b65f91716c7349fbb1
firefoxmmx2 <firefoxmmx@gmail.com>
2013-08-13 16:50:59 +0800

    还原为java 项目

---
+++ src/org/ffmmx/example/androidsqlite2/activity/NoteActivity.java
@@ -0,0 +1,16 @@
+package org.ffmmx.example.androidsqlite2.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoteActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
