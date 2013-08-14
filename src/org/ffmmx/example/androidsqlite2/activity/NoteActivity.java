bc80c83fa480200cfbdd2d628c4e7e1011efe294
firefoxmmx2 <firefoxmmx@gmail.com>
2013-08-14 13:58:05 +0800

    添加日程 添加对应字符串资源 添加对应布局资源

--- src/org/ffmmx/example/androidsqlite2/activity/NoteActivity.java
+++ src/org/ffmmx/example/androidsqlite2/activity/NoteActivity.java
@@ -1,16 +1,30 @@
 package org.ffmmx.example.androidsqlite2.activity;
 
+import org.ffmmx.example.androidsqlite2.R;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class NoteActivity extends Activity {
+	private EditText subjectEdit, contentEdit, noteTimeEdit;
+	private Button dateBtn, addNoteBtn;
+	private ListView noteListView;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	
-	    // TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.note_activity);
+
+		subjectEdit = (EditText) findViewById(R.id.note_subjectEdit);
+		contentEdit = (EditText) findViewById(R.id.note_contentEdit);
+		dateBtn = (Button) findViewById(R.id.note_dateSelectBtn);
+		addNoteBtn = (Button) findViewById(R.id.note_addNoteBtn);
+		noteListView = (ListView) findViewById(R.id.note_noteList);
+
 	}
 
 }
