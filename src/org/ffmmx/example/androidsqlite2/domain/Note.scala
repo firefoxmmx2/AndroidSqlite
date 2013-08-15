95dcb4aef1221a2abb78535d24afe41a544cc8d9
firefoxmmx2 <firefoxmmx2>
2013-08-16 00:22:53 +0800

    添加userid

--- src/org/ffmmx/example/androidsqlite2/domain/Note.scala
+++ src/org/ffmmx/example/androidsqlite2/domain/Note.scala
@@ -2,6 +2,6 @@ package org.ffmmx.example.androidsqlite2.domain
 
 import java.util.Date
 
-case class Note(id: Int = 0, subject: String, content: String, noteDate: Date) {
+case class Note(id: Int = 0, subject: String, content: String, noteDate: Date, userid: Int) {
 
 }
