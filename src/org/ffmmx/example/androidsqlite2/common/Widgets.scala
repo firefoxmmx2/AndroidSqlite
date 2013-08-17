aa973323f568116911cd6cc50f194472d6fde963
firefoxmmx2 <firefoxmmx2>
2013-08-17 18:20:16 +0800

    给findViewById添加类型边界

--- src/org/ffmmx/example/androidsqlite2/common/Widgets.scala
+++ src/org/ffmmx/example/androidsqlite2/common/Widgets.scala
@@ -5,19 +5,19 @@ import android.view.View
 import android.app.Activity
 
 object Widgets {
-	trait RichButton extends Button {
-		def onClick(handler: => Any) = {
-			this.setOnClickListener(new View.OnClickListener() {
-				override def onClick(v: View): Unit = {
-					handler
-				}
-			})
-		}
-	}
+  trait RichButton extends Button {
+    def onClick(handler: => Any) = {
+      this.setOnClickListener(new View.OnClickListener() {
+        override def onClick(v: View): Unit = {
+          handler
+        }
+      })
+    }
+  }
 
-	trait RichActivity extends Activity {
-		def getById[T](id: Int): T = {
-			this.findViewById(id).asInstanceOf[T]
-		}
-	}
+  trait RichActivity extends Activity {
+    def getById[T <: View](id: Int): T = {
+      this.findViewById(id).asInstanceOf[T]
+    }
+  }
 }
