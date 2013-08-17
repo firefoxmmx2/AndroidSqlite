8c66608f9b4e3c3e0e716bc211ad2775cd652b8a
firefoxmmx2 <firefoxmmx2>
2013-08-18 00:12:47 +0800

    更变从RichButton 为 隐式转换的Button

--- src/org/ffmmx/example/androidsqlite2/activity/LoginActivity.scala
+++ src/org/ffmmx/example/androidsqlite2/activity/LoginActivity.scala
@@ -10,47 +10,48 @@ import android.widget.EditText
 import org.ffmmx.example.androidsqlite2.common.SqlUtil
 import android.database.sqlite.SQLiteDatabase
 import android.widget.Toast
+import android.widget.Button
 
 class LoginActivity extends RichActivity {
-	override def onCreate(savedInstanceState: Bundle): Unit = {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_login)
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_login)
 
-		if (Login.isLogon()) {
-			val intent = new Intent(LoginActivity.this, classOf[NoteActivity])
-			startActivity(intent)
-			finish()
-		}
-		val sqlUtil = new SqlUtil(LoginActivity.this)
-		val usernameEdit = findViewById(R.id.usernameEdit).asInstanceOf[EditText]
-		val passwordEdit = findViewById(R.id.passwordEdit).asInstanceOf[EditText]
-		val loginBtn = findViewById(R.id.loginBtn).asInstanceOf[RichButton]
-		val registerBtn = findViewById(R.id.registerBtn).asInstanceOf[RichButton]
+    if (Login.isLogon()) {
+      val intent = new Intent(LoginActivity.this, classOf[NoteActivity])
+      startActivity(intent)
+      finish()
+    }
+    val sqlUtil = new SqlUtil(LoginActivity.this)
+    val usernameEdit = getById[EditText](R.id.usernameEdit)
+    val passwordEdit = getById[EditText](R.id.passwordEdit)
+    val loginBtn = getById[Button](R.id.loginBtn)
+    val registerBtn = getById[Button](R.id.registerBtn)
 
-		loginBtn.onClick({
-			val db = sqlUtil.getReadableDatabase()
+    loginBtn.onClick({
+      val db = sqlUtil.getReadableDatabase()
 
-			try {
-				val result = Login.login(db, usernameEdit.getText().toString(),
-					passwordEdit.getText().toString())
-				if (result) {
-					val intent = new Intent(LoginActivity.this,
-						classOf[NoteActivity])
-					startActivity(intent)
-					finish()
-				} else {
-					Toast.makeText(LoginActivity.this, "用户名不存在或者密码错误",
-						Toast.LENGTH_LONG).show()
-				}
-			} finally {
-				db.close()
-			}
+      try {
+        val result = Login.login(db, usernameEdit.getText().toString(),
+          passwordEdit.getText().toString())
+        if (result) {
+          val intent = new Intent(LoginActivity.this,
+            classOf[NoteActivity])
+          startActivity(intent)
+          finish()
+        } else {
+          Toast.makeText(LoginActivity.this, "用户名不存在或者密码错误",
+            Toast.LENGTH_LONG).show()
+        }
+      } finally {
+        db.close()
+      }
 
-		})
+    })
 
-		registerBtn.onClick({
-			val intent = new Intent(LoginActivity.this, classOf[RegisterActivity])
-			startActivity(intent)
-		})
-	}
+    registerBtn.onClick({
+      val intent = new Intent(LoginActivity.this, classOf[RegisterActivity])
+      startActivity(intent)
+    })
+  }
 }
--- src/org/ffmmx/example/androidsqlite2/activity/NoteActivity.scala
+++ src/org/ffmmx/example/androidsqlite2/activity/NoteActivity.scala
@@ -1,31 +1,38 @@
 package org.ffmmx.example.androidsqlite2.activity
 
-import android.app.Activity
-import android.os.Bundle
-import org.ffmmx.example.androidsqlite2.R
-import android.widget.EditText
-import org.ffmmx.example.androidsqlite2.common.Widgets._
+import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.HashMap
+
+import scala.collection.mutable.Map
+
+import org.ffmmx.example.androidsqlite2.R
+import org.ffmmx.example.androidsqlite2.business.Note
+import org.ffmmx.example.androidsqlite2.common.SqlUtil
+import org.ffmmx.example.androidsqlite2.common.Widgets._
+
+import android.app.Activity
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
+import android.os.Bundle
+import android.widget.Button
 import android.widget.DatePicker
-import java.text.SimpleDateFormat
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.SimpleAdapter
-import scala.collection.mutable.ListBuffer
-import scala.collection.mutable.Map
 
 class NoteActivity extends RichActivity {
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.note_activity)
 
+    val sqlUtil = new SqlUtil(NoteActivity.this)
     val subjectEdit = getById[EditText](R.id.note_subjectEdit)
     val contentEdit = getById[EditText](R.id.note_contentEdit)
     val noteTimeEdit = getById[EditText](R.id.note_noteTime)
-    val dateBtn = getById[RichButton](R.id.note_dateSelectBtn)
-    val addNoteBtn = getById[RichButton](R.id.note_addNoteBtn)
+    val dateBtn = getById[Button](R.id.note_dateSelectBtn)
+    val addNoteBtn = getById[Button](R.id.note_addNoteBtn)
     val noteListView = getById[ListView](R.id.note_noteList)
 
     dateBtn.onClick({
@@ -43,9 +50,25 @@ class NoteActivity extends RichActivity {
     })
 
     //初始化列表
-    val noteList = ListBuffer[Map[String, Any]]()
+    val noteListViewData = new java.util.ArrayList[java.util.Map[String, Object]]()
+    val db = sqlUtil.getReadableDatabase()
+    try {
+      val notelist = Note.all(db)
+      notelist.foreach(note => {
+        val map = new HashMap[String, Object]()
+        map.put("itemId", new Integer(note.id))
+        map.put("itemSubject", note.subject)
+        map.put("itemContent", note.content)
+        map.put("itemNotetime", note.noteDate)
+        noteListViewData.add(map)
+      })
+    } catch {
+      case t: Throwable => println(t)
+    } finally {
+      db.close()
+    }
 
-    val simpleAdapter = new SimpleAdapter(NoteActivity.this, noteList.,
+    val simpleAdapter = new SimpleAdapter(NoteActivity.this, noteListViewData,
       R.layout.note_list,
       Array("itemSubject", "itemContent", "itemNotetime"),
       Array(R.id.notelist_itemSubject, R.id.notelist_itemContent,
